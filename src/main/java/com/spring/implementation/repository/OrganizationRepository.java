1f0788b7b27d88321e374e7c22d8e16f8e6f1f8f
GOPI KRISHNA <2300030503@kluniversity.in>
2025-06-05 21:13:05 +0530

    feature/dbintegration first commit

---
+++ src/main/java/com/spring/implementation/repository/OrganizationRepository.java
@@ -0,0 +1,12 @@
+package com.spring.implementation.repository;
+
+import com.spring.implementation.model.Organization;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface OrganizationRepository extends JpaRepository<Organization, Long> {
+    Optional<Organization> findByEmail(String email);
+}
